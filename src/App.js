35012512175d08c4aa7b06393fe2d78743b46412
antimech <antimech666@gmail.com>
2018-05-14 21:20:41 +0600

    First steps to add chat's layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,95 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
 
-class App extends Component {
+const drawerWidth = 320;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    height: 430,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+  },
+  'appBar-left': {
+    marginLeft: drawerWidth,
+  },
+  'appBar-right': {
+    marginRight: drawerWidth,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class PermanentDrawer extends React.Component {
   render() {
+    const { classes } = this.props;
+
+    const drawer = (
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <TextField
+            fullWidth
+            placeholder="Search chats..."
+            margin="normal"
+          />
+        </div>
+        <Divider />
+        <h3>There is no chats yet...</h3>
+      </Drawer>
+    );
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div className={classes.appFrame}>
+        <AppBar
+          position="absolute"
+          className={classes.appBar}
+        >
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              DogeCodes React Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {drawer}
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography>{'You think water moves fast? You should see ice.'}</Typography>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(PermanentDrawer);
